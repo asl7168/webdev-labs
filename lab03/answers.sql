61e05dbd386636d0049eabc572fa7446738f053c
asl7168 <adamleif2024@u.northwestern.edu>
2022-04-15 14:26:21 -0500

    COMPLETED (might just wait to see solutions if post not answered in time)

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -10,42 +10,63 @@ FROM users;
 
 
 -- Exercise 3
-
+SELECT id, first_name, last_name
+FROM users
+ORDER BY last_name;
 
 
 
 -- Exercise 4
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 26;
 
 
 
 -- Exercise 5
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 12 OR user_id = 26;
 
 
 
 -- Exercise 6
-
+SELECT COUNT(*)
+FROM posts;
 
 
 
 -- Exercise 7
-
+SELECT user_id, COUNT(*)
+FROM comments
+GROUP BY user_id
+ORDER BY COUNT(*) DESC;
 
 
 
 -- Exercise 8
-
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name
+FROM posts 
+JOIN users ON posts.user_id = users.id
+WHERE posts.user_id = 12 OR posts.user_id = 26;
 
 
 
 -- Exercise 9
-
+SELECT p.id, p.pub_date, f.following_id
+FROM posts AS p
+JOIN following AS f ON p.user_id=f.following_id
+WHERE f.user_id=26;
 
 
 
 -- Exercise 10
-
+SELECT p.id, p.pub_date, f.following_id, u.username
+FROM posts AS p
+JOIN following AS f ON p.user_id=f.following_id
+JOIN users AS u ON f.following_id=u.id
+WHERE f.user_id=26
+ORDER BY p.pub_date DESC;
 
 
 
